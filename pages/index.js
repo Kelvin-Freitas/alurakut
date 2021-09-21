c39c2de9fd976836b8d9e947b8a55c0e21cd5459
Kelvin Freitas <kelvin.arg11@gmail.com>
2021-09-21 01:25:29 -0300

    aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
+import React from 'react'
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSideBar(propriedades) {
-  console.log(propriedades.gitHubUser);
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${propriedades.gitHubUser}.png`} style={{ borderRadius: '8px' }} />
+      
+      <hr/>
+      <p> 
+        <a class="boxLink" href={`https://gituhub.com/${propriedades.gitHubUser}`}>
+          @{propriedades.gitHubUser}
+        </a>
+      </p>
+      <hr/>
+      
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
+  const [comunidades,setComunidades] = React.useState({
+    id: new Date().toISOString(),
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  });
   const user = 'juunegreiros';
   const pessoasFavoritas = [
     'juunegreiros',
@@ -36,8 +51,58 @@ export default function Home() {
             <h1 className="title">Bem Vindo(a)</h1>
             <OrkutNostalgicIconSet/>
           </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer ?</h2>
+            <form onSubmit={function handleCriaComunidade(e){
+                e.preventDefault();
+                const dados = new FormData(e.target);
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dados.get('title'),
+                  image: dados.get('image'),
+                };
+                const atualizadas = [...comunidades, comunidade];
+                setComunidades(atualizadas);
+            }}>
+              <div>
+                <input 
+                  placeholder="Qual vai ser o nome da sua comunidade ?" 
+                  name="title" 
+                  aria-label="Qual vai ser o nome da sua comunidade ?" 
+                  type="text"
+                />
+              </div>
+              <div>
+                <input 
+                  placeholder="Coloque um URL para usarmos de capa" 
+                  name="image" 
+                  aria-label="Coloque um URL para usarmos de capa" 
+                />
+              </div>
+              <button>
+                Criar Comunidade
+              </button>
+            </form>
+          </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Comunidades ({comunidades.length})
+            </h2>
+            <ul>
+              {comunidades.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <a href={`/users/${item.title}`}>
+                      <img src={item.image} />
+                      <span>{item.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+        </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da Comunidade ({pessoasFavoritas.length})
@@ -45,8 +110,8 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((item) => {
                 return (
-                  <li>
-                    <a href={`/users/${item}`} key={item}>
+                  <li key={item}>
+                    <a href={`/users/${item}`} >
                       <img src={`https://github.com/${item}.png`} />
                       <span>{item}</span>
                     </a>
